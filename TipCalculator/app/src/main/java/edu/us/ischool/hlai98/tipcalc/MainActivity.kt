79369faa0cccb7cf563f281e9f719b1c2d04345a
YvonneLaiii <hlai98@uw.edu>
2019-04-22 22:26:46 -0700

    finish

--- TipCalculator/app/src/main/java/edu/us/ischool/hlai98/tipcalc/MainActivity.kt
+++ TipCalculator/app/src/main/java/edu/us/ischool/hlai98/tipcalc/MainActivity.kt
@@ -1,39 +1,59 @@
 package edu.us.ischool.hlai98.tipcalc
 
+import android.content.Context
+import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
-import android.view.Menu
-import android.view.MenuItem
-
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.*
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.text.NumberFormat
+import java.util.*
+import android.view.Menu;
 
 class MainActivity : AppCompatActivity() {
-
+    private var input : String = ""
+    private val cur : Regex = Regex("[$,.]")
+    private var percentage : Double = 0.15
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        val format = NumberFormat.getCurrencyInstance(Locale.US)
+        val entered =findViewById<EditText>(R.id.editText)
+        val button = findViewById<Button>(R.id.button)
+        button.isEnabled = false
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        entered.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                button.isEnabled = true
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.toString().equals(input)) {
+                    entered.removeTextChangedListener(this)
+                    var temp = s.toString().replace(cur, "")
+                    var number = temp.toDouble()
+                    var formatted = format.format((number / 100))
+                    entered.setText(formatted)
+                    entered.setSelection(formatted.length)
+                    input = formatted
+                    entered.addTextChangedListener(this)
+                }
+            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+        })
+        button.setOnClickListener{
+            var amount = ((entered.text.toString().replace(cur, "")).toDouble() / 100) * percentage
+            toast(format.format(amount))
         }
     }
-
+    private fun Context.toast(message: CharSequence) = Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 }
